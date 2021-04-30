96958ca1568ee86c09d26b012015032605e5a467
Elizabeth Sarduy <elizabeth_s7@ymail.com>
2021-04-29 23:58:05 -0400

    Portfolio 2

--- lab11/index.js
+++ lab11/index.js
@@ -1,8 +1,11 @@
 import MongoClient from 'mongodb';
+import chalk from "chalk";
+import timestamp from 'time-stamp';
+
 
 const URL = "mongodb+srv://esarduyrodriguez:UFCZGeA55T7ZnT6N@cluster0.oewwc.mongodb.net"; //connect to database
 
-// gather and list specified data
+// gather and list airbnb data
 MongoClient.connect(URL, {useUnifiedTopology: true})
 .then(connection => {
   let database = connection.db("sample_airbnb");
@@ -12,12 +15,32 @@ MongoClient.connect(URL, {useUnifiedTopology: true})
   let cursor = collection.find({"review_scores.review_scores_rating": {$gte: 99}, beds: {$gte: 5}, price: {$lte: 200}});
 
   cursor.forEach(document => {
-    console.log(document.name);
+    console.log(chalk.white.bgGreen(document.name)); // green background with white text
+    
   }, () => {
     connection.close();
+    console.log(timestamp.utc('HH:mm:ss')); //Logs time stamp
+  })
+}) 
+
+//gather and list restaurant data
+MongoClient.connect(URL, {useUnifiedTopology: true})
+.then(connection => {
+  let database = connection.db("sample_restaurants");
+
+  let collection = database.collection("restaurants");
+
+  let cursor = collection.find({cuisine: "Bakery"},{borough: "Brooklyn"},{ grade: "A" }, {score: {$gte:500} });
+
+  cursor.forEach(document => {
+    console.log(chalk.white.bgRed(document.name)); //chalk-red background and white text
+    
+  }, () => {
+    connection.close();
+    console.log(timestamp.utc('HH:mm:ss')); //Logs time stamp
   })
 }) 
 // if an error occurs 
 .catch(error => {
-  console.log("Error: " + error);
+  console.log(chalk.red.bold("Error: " + error)); //chalk-red bold letters
 })
